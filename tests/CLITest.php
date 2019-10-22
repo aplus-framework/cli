bf541847deecf3c9a0501d6b16d392120a6484cf
Natan Felles <natanfelles@gmail.com>
2019-10-22 15:55:00 -0300

    Add tests for CLI

--- tests/CLITest.php
+++ tests/CLITest.php
@@ -20,4 +20,58 @@ class CLITest extends TestCase
 		CLI::write('Hello!');
 		$this->assertEquals(Stream::$output, "Hello!\n");
 	}
+
+	public function testBeep()
+	{
+		CLI::beep(2);
+		$this->assertEquals(Stream::$output, "\x07\x07");
+	}
+
+	public function testNewLine()
+	{
+		CLI::newLine(2);
+		$this->assertEquals(Stream::$output, \PHP_EOL . \PHP_EOL);
+	}
+
+	public function testIsWindows()
+	{
+		$this->assertFalse(CLI::isWindows());
+	}
+
+	public function testWidth()
+	{
+		$this->assertEquals(80, CLI::getWidth());
+	}
+
+	public function testWrap()
+	{
+		$line[0] = \str_repeat('a', 80);
+		$line[1] = \str_repeat('a', 80);
+		$line[2] = \str_repeat('a', 80);
+		$this->assertEquals(
+			$line[0] . \PHP_EOL . $line[1] . \PHP_EOL . $line[2],
+			CLI::wrap(\implode($line))
+		);
+	}
+
+	public function testClear()
+	{
+		CLI::clear();
+		$this->assertEquals(Stream::$output, "\e[H\e[2J");
+	}
+
+	public function testTable()
+	{
+		CLI::table([[1, 'John'], [2, 'Mary']], ['ID', 'Name']);
+		$table = <<<EOL
++----+------+
+| ID | Name |
++----+------+
+| 1  | John |
+| 2  | Mary |
++----+------+
+
+EOL;
+		$this->assertEquals(Stream::$output, $table);
+	}
 }
