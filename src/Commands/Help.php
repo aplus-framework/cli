4f6ff30a7cc631aeb8593d327d8b2bf09ea80d22
Natan Felles <natanfelles@gmail.com>
2021-06-26 19:21:06 -0300

    Update Coding Standard - add header comments

--- src/Commands/Help.php
+++ src/Commands/Help.php
@@ -1,4 +1,13 @@
-<?php namespace Framework\CLI\Commands;
+<?php
+/*
+ * This file is part of The Framework CLI Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\CLI\Commands;
 
 use Framework\CLI\CLI;
 use Framework\CLI\Command;
--- src/Languages/es/cli.php
+++ src/Languages/es/cli.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of The Framework CLI Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 return [
 	'command' => 'Comando',
 	'commandNotFound' => 'Comando no encontrado: "{0}"',
