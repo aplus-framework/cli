cbec840895be20e6d02c823d42284f69d82c00e0
Natan Felles <natanfelles@gmail.com>
2021-07-14 13:18:46 -0300

    Update header comment on .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,4 +1,13 @@
-<?php namespace PHPSTORM_META;
+<?php
+/*
+ * This file is part of Aplus Framework CLI Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPSTORM_META;
 
 registerArgumentsSet(
     'cli_foreground_colors',
